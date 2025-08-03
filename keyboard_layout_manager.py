a56bcfaa012149f6692310edebc1b469e862474c
Angel Hasbun <angelhasbun14@gmail.com>
2025-08-03 11:52:13 -0600

    prueba numero 1 de union de codigos, creacion de Keyboard_layout y score manager

---
+++ keyboard_layout_manager.py
@@ -0,0 +1,9 @@
+# keyboard_layout_manager.py
+class KeyboardLayoutManager:
+    def __init__(self):
+        # Inicializa tu manejador de teclado aquí
+        pass
+
+    # Agrega aquí los métodos relacionados con el teclado, por ejemplo:
+    # def get_current_layout(self):
+    #     return "qwerty"
---
+++ score_manager.py
@@ -0,0 +1,12 @@
+# score_manager.py
+class ScoreManager:
+    def __init__(self):
+        # Inicializa tu manejador de puntuaciones aquí
+        pass
+
+    # Agrega aquí los métodos relacionados con la puntuación, por ejemplo:
+    def add_score(self, points):
+        pass
+
+    def get_score(self):
+        return 0
